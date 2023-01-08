b27879e78f4a80cc4d4219dca3973397fc323adf
Deo Kalule <deo.kalule@immertec.com>
2023-01-08 18:29:28 -0500

    Finished merge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import logo from "./logo.svg";
-import background from "./backgrounds/tall_scotch_bottle.jpg";
+import groom_background from "./backgrounds/tall_scotch_bottle.jpg";
+import bride_background from "./backgrounds/champagne.jpg";
 import "./App.css";
+
+const WEDDING_PARTY_TYPES = {
+  GROOM: 0,
+  BRIDE: 1,
+};
+
+const WEDDING_PARTY = WEDDING_PARTY_TYPES.BRIDE;
+
+function getBackground() {
+  switch (WEDDING_PARTY) {
+    case WEDDING_PARTY_TYPES.BRIDE:
+      return bride_background;
+    default:
+      return groom_background;
+  }
+}
+
+function getBackgroundClassName() {
+  switch (WEDDING_PARTY) {
+    case WEDDING_PARTY_TYPES.BRIDE:
+      return "poppy-App-bg";
+    default:
+      return "App-bg";
+  }
+}
 
 function App() {
   return (
     <div className="App" className="App-Bg-Div">
-      <img src={background} className="App-bg" />
+      <img src={getBackground()} className={getBackgroundClassName()} />
       <h1 className="App-title">Wedding Party</h1>
     </div>
   );
