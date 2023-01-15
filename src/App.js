9b5718bd6c062f515bfc838e82700755f1a3a21b
Godfrey Kirumira <godfrey@pnautomation.com>
2023-01-15 16:10:03 -0500

    Fixed Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import background from "./backgrounds/tall_scotch_bottle.jpg";
 import "./App.css";
 
+const events = [
+  {
+    title: "Lorem ipsum 1",
+    desc: "Lorem ipsum dolor sit amet",
+    time: "02-05-2023",
+  },
+  {
+    title: "Lorem ipsum 2",
+    desc: "Lorem ipsum dolor sit amet",
+    time: "02-05-2023",
+  },
+  {
+    title: "Lorem ipsum 3",
+    desc: "Lorem ipsum dolor sit amet",
+    time: "02-05-2023",
+  },
+  {
+    title: "Lorem ipsum 4",
+    desc: "Lorem ipsum dolor sit amet",
+    time: "02-05-2023",
+  },
+];
+
+function Cell(item) {
+  return <h1>{item.title}</h1>;
+}
+
 function App() {
   return (
-    <div className="App" className="App-Bg-Div">
-      <img src={background} className="App-bg" />
+    <div className="App-Bg-Div">
+      <img src={background} className="App-bg" alt="bg" />
       <h1 className="App-title">Wedding Party</h1>
+      <h1 className="App-list">
+        {events.map((item, index) => (
+          <h1 key={index} item={item}>
+            {item.title}
+          </h1>
+        ))}
+      </h1>
     </div>
   );
 }
